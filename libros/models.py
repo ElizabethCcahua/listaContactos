28f2850f12cfca8fb94a658878ef82b2330c776f
ElizabethCCahua <eccahuach@unsa.edu.pe>
2024-11-27 07:21:01 -0500

    creacion modelo clase review

--- libros/models.py
+++ libros/models.py
@@ -10,3 +10,10 @@ class Book(models.Model):
     publication_date = models.DateField()
     cover_image = models.ImageField(upload_to='book_covers', null=True, blank=True)
 
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField(choices=[(i, i) for i in range(1, 6)])
+    text = models.TextField()
+    comments = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
