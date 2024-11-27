9e9724f3b03459ed090861b84ce0f932ce31a2f8
ElizabethCCahua <eccahuach@unsa.edu.pe>
2024-11-27 07:44:07 -0500

    para egistrar modelos Book y Review en el administrador de Django

--- libros/admin.py
+++ libros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Review  # nombre del modelo
+
+# Registro simple de los modelos
+admin.site.register(Book)
+admin.site.register(Review)
