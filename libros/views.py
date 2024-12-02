e0e547c14014fb1f9088226727c82910c508721c
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 07:33:39 -0500

    implementamos una vista para las resenas  en views.py de libros

--- libros/views.py
+++ libros/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from .forms import ReviewForm
 from .forms import BookForm
+from .models import Book
 from django.contrib.auth.decorators import login_required
+
 
 # Vista para agregar un nuevo libro
 @login_required
@@ -19,3 +22,21 @@ def agregar_libro(request):
 def lista_libros(request):
     libros = Book.objects.all()
     return render(request, 'libros/lista_libros.html', {'libros': libros})
+
+
+# Vista para agregar una reseña a un libro
+@login_required
+def agregar_review(request, libro_id):
+    libro = Book.objects.get(id=libro_id)
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user  # Asocia la reseña con el usuario logueado
+            review.save()
+            return redirect('detalle_libro', libro_id=libro.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'libros/agregar_review.html', {'form': form, 'libro': libro})
