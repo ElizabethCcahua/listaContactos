45ffce60e6843a3c31c372acb8547df51132b6f8
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 07:52:38 -0500

    integrando las urls finales

--- personas/urls.py
+++ personas/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('admin/', admin.site.urls),
+    path('libros/', include('libros.urls')),
+    path('personas/', include('personas.urls')),
 ]
