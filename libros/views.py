e83c0933dd4d3728ea215a8a0d71d8d2ef12297e
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 07:16:58 -0500

    vista que permitira a los usuarios agrgegar un libro

--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Vista para agregar un nuevo libro
+@login_required
+def agregar_libro(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_libros')  # Redirige a la lista de libros
+    else:
+        form = BookForm()
+    
+    return render(request, 'libros/agregar_libro.html', {'form': form})
+
+# Vista para ver todos los libros
+def lista_libros(request):
+    libros = Book.objects.all()
+    return render(request, 'libros/lista_libros.html', {'libros': libros})
