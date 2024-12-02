59102ecde3924b70f8696510bf8ac37037ad8ce6
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 07:30:57 -0500

    se agrega el formulario de resena

--- libros/forms.py
+++ libros/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Book
+from .models import Review
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'description', 'publication_date']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['book', 'rating', 'text', 'comments']
