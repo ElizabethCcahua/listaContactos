4247c0cb24f696559221e4c88de54b76a5284c31
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 07:35:18 -0500

    gargamos una ruta para las resenas en urls.py de libros

--- libros/urls.py
+++ libros/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('agregar/', views.agregar_libro, name='agregar_libro'),
+    path('review/<int:libro_id>/', views.agregar_review, name='agregar_review'),
     path('', views.lista_libros, name='lista_libros'),
 ]
